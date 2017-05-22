269d1eaa69962d73495f11d5f9af3c58783edb85
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-22 00:20:13 -0700

    Add gulp tasks and make the app work

--- js/pingpong-interface.js
+++ js/pingpong-interface.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
-    var output = pingPong(goal);
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(goal);
     output.forEach(function(element) {
       $('#solution').append("<li>" + element + "</li>");
     });
