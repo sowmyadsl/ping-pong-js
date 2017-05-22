fd3158acf25a59a54b625eaa685582b6405cd437
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-22 00:15:24 -0700

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,47 @@
-var gulp = require('gulp');
+var del = require('del');
+var utilities = require('gulp-util');
+var gulp = require ('gulp');
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
-gulp.task('myTask', function(){
-  console.log('hello gulp');
+var buildProduction = utilities.env.production;
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/*-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
